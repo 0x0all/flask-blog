9b6f1652346c7cb28ebad717b885b9fe1334ca7a
yakiang <strak47@gmail.com>
2014-01-11 19:56:51 +0800

    add comments

--- src/blog/config.py
+++ src/blog/config.py
@@ -1,10 +1,14 @@
 #-*-coding:utf-8-*-
 
+# how many posts to display on home page
 PAGINATE = 10
 
+# path to sqlite db file
 DBFILE = '/var/www/flask-blog/db/posts.db'
 
+# string to split the post info and post content
 INFO_CONTENT_SPLIT = '<!---->\n'
 
+# string to split post preview and post main body 
 PREVIEW_MORE_SPLIT = '<!--more-->\n'
 
