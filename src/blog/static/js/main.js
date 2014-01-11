21eca4bae281a269f2ac930660d0dc9740c75df7
yakiang <strak47@gmail.com>
2014-01-11 20:13:34 +0800

    add comments

--- src/blog/static/js/main.js
+++ src/blog/static/js/main.js
@@ -1,4 +1,5 @@
 window.onscroll = function() {
+    // display: none if on top
     document.getElementById('backtotop').style.display = (document.body.scrollTop) ? 'block' : 'none';
 }
 
@@ -6,9 +7,11 @@ window.onscroll = function() {
 var toTopImg = document.getElementById('backtotop');
 
 toTopImg.onclick = function backToTop() {
+    // scroll to top
     var scroll = setInterval(function() {
         window.scrollBy(0, -150);
         if (document.body.scrollTop === 0) {
+            // reach top
             clearInterval(scroll);
         }
     }, 10);
